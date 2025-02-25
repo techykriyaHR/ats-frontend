938adfee3faca9cf0b49f12325e96e0c5b6a4bc5
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-02-25 14:43:28 +0800

    feat: add applicant status in ATS healthcheck.

--- src/components/ATSHeathcheck.jsx
+++ src/components/ATSHeathcheck.jsx
@@ -17,92 +17,108 @@ const applicants = [
     name: "user1",
     updated_at: new Date("2025-01-20"),
     position_applied: "GIS Analyst",
+    status: "first stage",
   },
   {
     id: 2,
     name: "user2",
     updated_at: new Date("2025-02-21"),
     position_applied: "GIS Analyst",
+    status: "second stage",
   },
   {
     id: 3,
     name: "user3",
     updated_at: new Date("2025-01-22"),
     position_applied: "Data Scientist",
+    status: "interview stage",
   },
   {
     id: 4,
     name: "user4",
     updated_at: new Date("2025-01-23"),
     position_applied: "Software Engineer",
+    status: "first stage",
   },
   {
     id: 5,
     name: "user5",
     updated_at: new Date("2025-01-24"),
     position_applied: "Backend Developer",
+    status: "second stage",
   },
   {
     id: 6,
     name: "user6",
     updated_at: new Date("2025-01-25"),
     position_applied: "Frontend Developer",
+    status: "interview stage",
   },
   {
     id: 7,
     name: "user7",
     updated_at: new Date("2025-01-26"),
     position_applied: "GIS Analyst",
+    status: "first stage",
   },
   {
     id: 8,
     name: "user8",
     updated_at: new Date("2025-02-13"),
     position_applied: "Full-Stack Developer",
+    status: "second stage",
   },
   {
     id: 9,
     name: "user9",
     updated_at: new Date("2025-02-15"),
     position_applied: "Project Manager",
+    status: "interview stage",
   },
   {
     id: 10,
     name: "user10",
     updated_at: new Date("2025-01-29"),
     position_applied: "GIS Technician",
+    status: "first stage",
   },
   {
     id: 11,
     name: "user11",
     updated_at: new Date("2025-01-30"),
     position_applied: "DevOps Engineer",
+    status: "second stage",
   },
   {
     id: 12,
     name: "user12",
     updated_at: new Date("2025-01-31"),
     position_applied: "Database Administrator",
+    status: "interview stage",
   },
   {
     id: 13,
     name: "user13",
     updated_at: new Date("2025-02-01"),
     position_applied: "Network Engineer",
+    status: "first stage",
   },
   {
     id: 14,
     name: "user14",
     updated_at: new Date("2025-02-02"),
     position_applied: "GIS Developer",
+    status: "second stage",
   },
   {
     id: 15,
     name: "user15",
     updated_at: new Date("2025-02-03"),
     position_applied: "QA Engineer",
+    status: "interview stage",
   },
 ];
+
 
 function UserCard({ applicant, daySelected }) {
   const currentDate = format(new Date(), 'yyyy-MM-dd')
@@ -111,15 +127,16 @@ function UserCard({ applicant, daySelected }) {
 
   if (daysElapsed >= daySelected) {
     return (
-      <div className="flex justify-between rounded-md border-1 p-2">
+      <div className="flex justify-between rounded-2xl border-2 border-gray-200 p-2 hover:bg-gray-100 cursor-pointer">
         <div>
-          <p className="text-sm font-semibold">{applicant.name}</p>
+          <p className="text-md font-semibold">{applicant.name}</p>
           <p className="text-xs text-gray-500">{applicant.position_applied}</p>
         </div>
 
-        <div className="flex">
-          <p className="text-xs">Last Updated {daysElapsed} days ago | </p>
-          <p className="text-xs text-green-700">Reply</p>
+        <div className="flex flex-col items-end">
+          <p className="text-md font-semibold">{applicant.status}</p>
+          <p className="text-xs" style={{color: "#008080"}}>Last Updated {daysElapsed} days ago | Reply</p> 
+
         </div>
       </div>
     );
@@ -136,7 +153,7 @@ function ATSHealthcheck() {
       <div className="flex justify-between">
         <h2 className="text-lg font-semibold">ATS Healthcheck</h2>
         <div>
-          <select className="bg-gray-200 rounded-md px-5" onChange={(e) => setDaySelected(e.target.value)}>
+          <select className="rounded-md px-5 border-2 border-gray-200" onChange={(e) => setDaySelected(e.target.value)}>
             {days.map((day) => {
               return (
                 <option value={day.day} key={day.id}>{day.day}</option>
@@ -146,7 +163,7 @@ function ATSHealthcheck() {
         </div>
       </div>
 
-      <div className="flex flex-col overflow-y-auto min-h-0 space-y-2 mt-4">
+      <div className="flex flex-col overflow-y-auto min-h-0 space-y-2 no-scrollbar">
         {applicants.map((applicant) => (
           <UserCard applicant={applicant} key={applicant.id} daySelected={daySelected} />
         ))}
