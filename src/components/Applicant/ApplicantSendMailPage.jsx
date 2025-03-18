6cbf745a30ade2075a39807b90e459dccb87575b
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-03-18 16:38:54 +0800

    feat: Add functionality to save email templates and convert HTML to JSON format

--- src/components/Applicant/ApplicantSendMailPage.jsx
+++ src/components/Applicant/ApplicantSendMailPage.jsx
@@ -6,6 +6,7 @@ import TextAlign from "@tiptap/extension-text-align";
 import Strike from "@tiptap/extension-strike";
 import CodeBlock from "@tiptap/extension-code-block";
 import Blockquote from "@tiptap/extension-blockquote";
+import { generateJSON } from "@tiptap/html"; // Import the utility to convert HTML to JSON
 import {
   BoldIcon,
   ItalicIcon,
@@ -22,9 +23,12 @@ import {
   Bars3BottomRightIcon,
 } from "@heroicons/react/24/outline";
 import api from "../../api/axios";
+import { sub } from "date-fns";
 
 function ApplicantSendMailPage() {
-  const [subject, setSubject] = useState("Welcome to FullSuite – Preparing for Your Interviews and Assessment");
+  const [subject, setSubject] = useState(
+    "Welcome to FullSuite – Preparing for Your Interviews and Assessment",
+  );
   const [attachment, setAttachment] = useState(null);
   const [emailContent, setEmailContent] = useState("");
   const [templates, setTemplates] = useState([]);
@@ -61,9 +65,36 @@ function ApplicantSendMailPage() {
     if (template) {
       setSelectedTemplate(selectedTitle);
       setSubject(template.subject); // Update the subject
-      editor?.commands.setContent(template.body); // Update the editor content
+
+      // Convert the HTML content to JSON format for the editor
+      const jsonContent = generateJSON(template.body, [
+        StarterKit,
+        Underline,
+        Strike,
+        CodeBlock,
+        Blockquote,
+        TextAlign.configure({ types: ["heading", "paragraph"] }),
+      ]);
+
+      editor?.commands.setContent(jsonContent); // Update the editor content
       setEmailContent(template.body);
     }
+  };
+
+  const handleSaveTemplate = () => {
+    //payload
+    const data = {
+      company_id: "468eb32f-f8c1-11ef-a725-0af0d960a833", 
+      title: "benzzzzz", 
+      subject: subject, 
+      body: emailContent, 
+    }
+
+    api.post('/email/add/template', data).then((response) => {
+      alert("added successfully"); 
+    }).catch((error) => {
+      alert("failed"); 
+    });
   };
 
   const handleSendEmail = async () => {
@@ -100,50 +131,104 @@ function ApplicantSendMailPage() {
   }
 
   return (
-    <div className="p-6 bg-gray-50 min-h-screen">
-      <div className="flex border border-gray-200 rounded-lg mb-5 overflow-hidden">
-        <span className="bg-teal-600 text-white px-4 py-2 rounded-l-lg">Subject</span>
+    <div className="min-h-screen bg-gray-50 p-6">
+      <div className="mb-5 flex overflow-hidden rounded-lg border border-gray-200">
+        <span className="rounded-l-lg bg-teal-600 px-4 py-2 text-white">
+          Subject
+        </span>
         <input
           type="text"
           value={subject}
           onChange={(e) => setSubject(e.target.value)}
-          className="flex-1 border-none bg-white focus-visible:ring-0 focus-visible:ring-offset-0 p-2 rounded-r-lg"
+          className="flex-1 rounded-r-lg border-none bg-white p-2 focus-visible:ring-0 focus-visible:ring-offset-0"
         />
       </div>
 
-      <div className="p-6 bg-white rounded-xl border border-gray-200 shadow-md mb-5">
-        <div className="flex gap-3 mb-4 shadow-lg p-3 rounded-lg bg-gray-100">
-          <BoldIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().toggleBold().run()} />
-          <ItalicIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().toggleItalic().run()} />
-          <UnderlineIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().toggleUnderline().run()} />
-          <StrikethroughIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().toggleStrike().run()} />
-          <ListBulletIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().toggleBulletList().run()} />
-          <NumberedListIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().toggleOrderedList().run()} />
-          <ChatBubbleLeftRightIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().toggleBlockquote().run()} />
-          <CodeBracketIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().toggleCodeBlock().run()} />
-          <ArrowUturnLeftIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().undo().run()} />
-          <ArrowUturnRightIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().redo().run()} />
-          <Bars3BottomLeftIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().setTextAlign("left").run()} />
-          <Bars3CenterLeftIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().setTextAlign("center").run()} />
-          <Bars3BottomRightIcon className="w-6 h-6 cursor-pointer" onClick={() => editor.chain().focus().setTextAlign("right").run()} />
+      <div className="mb-5 rounded-xl border border-gray-200 bg-white p-6 shadow-md">
+        <div className="mb-4 flex gap-3 rounded-lg bg-gray-100 p-3 shadow-lg">
+          <BoldIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().toggleBold().run()}
+          />
+          <ItalicIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+          />
+          <UnderlineIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().toggleUnderline().run()}
+          />
+          <StrikethroughIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+          />
+          <ListBulletIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+          />
+          <NumberedListIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          />
+          <ChatBubbleLeftRightIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          />
+          <CodeBracketIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          />
+          <ArrowUturnLeftIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().undo().run()}
+          />
+          <ArrowUturnRightIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().redo().run()}
+          />
+          <Bars3BottomLeftIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().setTextAlign("left").run()}
+          />
+          <Bars3CenterLeftIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().setTextAlign("center").run()}
+          />
+          <Bars3BottomRightIcon
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => editor.chain().focus().setTextAlign("right").run()}
+          />
         </div>
-        <EditorContent editor={editor} className="rounded-lg bg-white min-h-[500px] p-4 border border-gray-200" />
+        <EditorContent
+          editor={editor}
+          className="min-h-[500px] rounded-lg border border-gray-200 bg-white p-4"
+        />
       </div>
 
-      <div className="flex border border-gray-100 bg-white mb-5">
-        <label htmlFor="file-upload" className="cursor-pointer bg-teal-600 text-white text-center px-8 py-2 rounded-md">
+      <div className="mb-5 flex border border-gray-100 bg-white">
+        <label
+          htmlFor="file-upload"
+          className="cursor-pointer rounded-md bg-teal-600 px-8 py-2 text-center text-white"
+        >
           Attachment
-          <input id="file-upload" type="file" className="hidden" onChange={handleFileChange} />
+          <input
+            id="file-upload"
+            type="file"
+            className="hidden"
+            onChange={handleFileChange}
+          />
         </label>
-        <span className="text-gray-500 ml-3">{attachment ? attachment.name : "No file chosen"}</span>
+        <span className="ml-3 text-gray-500">
+          {attachment ? attachment.name : "No file chosen"}
+        </span>
       </div>
 
-      <div className="flex justify-between items-center">
-        <div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-4">
           <select
             value={selectedTemplate}
             onChange={handleTemplateSelect}
-            className="bg-teal-600 hover:bg-teal-700 text-white text-center px-4 py-2 rounded-md w-full"
+            className="rounded-md bg-teal-600 px-4 py-2 text-center text-white hover:bg-teal-700"
           >
             <option value="" disabled>
               Select a Template
@@ -154,11 +239,21 @@ function ApplicantSendMailPage() {
               </option>
             ))}
           </select>
+          <button
+            onClick={handleSaveTemplate}
+            className="rounded-md bg-white px-6 py-2 text-teal-600 shadow-md border border-teal-600 hover:bg-teal-700"
+          >
+            Save as Template
+          </button>
         </div>
-        <button onClick={handleSendEmail} className="bg-teal-600 hover:bg-teal-700 text-white px-6 py-2 rounded-md shadow-md">
+        <button
+          onClick={handleSendEmail}
+          className="rounded-md bg-teal-600 px-6 py-2 text-white shadow-md hover:bg-teal-700"
+        >
           Send
         </button>
       </div>
+
     </div>
   );
 }
