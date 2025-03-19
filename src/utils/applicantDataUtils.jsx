49344ec7f73af1c7ab10fbeefa4cb2e3e1f6fa3c
benz <lakycute2828@gmail.com>
2025-03-19 14:52:58 +0800

    fixed fetching all when no filter selected

--- src/utils/applicantDataUtils.jsx
+++ src/utils/applicantDataUtils.jsx
@@ -2,9 +2,6 @@ import api from "../api/axios";
 import { useApplicantData } from "../hooks/useApplicantData";
 
 export const fetchApplicants = async (setApplicantData) => {
-    // api.get(`/applicants`).then( response => {
-    //     setApplicantData(response.data)
-    // }).catch(error => console.error("Error fetching data:", error));
     const { data } = await api.get(`/applicants`);
     setApplicantData(data);
 }
@@ -14,14 +11,14 @@ export const filterApplicants = async (position, setApplicantData, status) => {
     let sql = "/applicants/filter?";
     position != "All" ? sql += `position=${position}` : sql += "";
     // status != null ? sql += `&status=${status}` : sql += "";
-    if (status == []) {
+    console.log(status);
+    if (status.length === 0 && position === "All") {
         fetchApplicants(setApplicantData);
-        return;
     }
-    else if (status != []) {
-        status.map((status) => {
-            sql += `&status=${status}`;
-        });
+    else {
+        status.forEach((statusItem) => {
+            sql += `&status=${statusItem}`;
+          });
         const { data } = await api.get(sql);
         setApplicantData(data);
     }
